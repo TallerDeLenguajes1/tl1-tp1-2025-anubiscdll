d6dd493b38ff11ec39f078c3810e0b0e8c5d96db
anubiscdll <anubisbalbo@gmail.com>
2026-04-01 11:56:08 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
-        printf("hola mundo\n");
+    printf("hola mundo\n");
 
+    int* puntero;
+    int valor = 5;
+    puntero = &valor;
+    printf("El valor contenido en el puntero es: %d\n", *puntero);
+    printf("la dirección de memoria almacenada en el puntero es: %p\n", puntero);
+    printf("la dirección de memoria de la variable valor es: %p\n", &valor);
+    printf("la dirección de memoria del puntero es: %p\n", &puntero);
+    printf("el tamaño de la variable valor es: %d bytes\n", sizeof(valor));
     return 0;
 }
